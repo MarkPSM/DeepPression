b4fb269a1097f158d72ea7fdfa45f25e08097766
MARCOS PAULO SANTANA MACEDO <43195320827@info.local>
2025-09-04 17:30:15 -0300

    feat: colliders

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -16,6 +16,21 @@ public class CharacterManager : MonoBehaviour
     public int Defense;
     public int Speed;
 
+    [Header("Movimentação")]
+    private Rigidbody2D rb;
+    private float moveSpeed = 1f;
+    public float currentSpeed;
+
+    public Vector2 playerDirection;
+
+    public bool isWalking = false;
+    //private Animator animator;
+    private bool facingRight = true;
+    //private bool facingUp = false;
+    //private bool facingDown = false;
+    //private bool facingLeft = false;
+
+
     private void Awake()
     {
         maxHP = 100;
@@ -30,10 +45,48 @@ public class CharacterManager : MonoBehaviour
         mentalAttack = 1;
         Defense = 1;
         Speed = 20;
+
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = Speed / 10;
+        currentSpeed = moveSpeed;
     }
 
     void Update()
     {
-        
+        PlayerMove();
+    }
+
+    void FixedUpdate()
+    {
+        if (playerDirection.x != 0 || playerDirection.y != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+
+            rb.MovePosition(rb.position + currentSpeed * Time.fixedDeltaTime * playerDirection);
+    }
+
+    void PlayerMove()
+    {
+        playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (playerDirection.x < 0 && facingRight)
+        {
+            Flip();
+        }
+        else if (playerDirection.x > 0 && !facingRight) {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        transform.Rotate(0, 180, 0);
     }
 }
